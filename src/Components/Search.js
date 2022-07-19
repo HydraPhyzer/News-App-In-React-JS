7c99d4182af7d386e418a7fcf92e54a255e695e7
Muhammad Zubair <2020cs601@student.uet.edu.pk>
2022-07-19 07:25:22 +0500

    Initial Commit

---
+++ src/Components/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Search.css'
+
+let Search=()=>
+{
+    return(
+        <div className='Search'>
+            <input type="text" placeholder='Type to Search ...'/>
+        </div>
+    )
+}
+export default Search
